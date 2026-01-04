973d012595124edcc4c98604a7fea9fea5e9fe3e
Apirak Panatkool <apirakb@gmail.com>
2026-01-04 08:57:08 +0700

    Refactor code style for consistency; add Highlight component for enhanced text emphasis in blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://apirak.com',
+  site: "https://apirak.com",
   integrations: [sitemap(), icon(), mdx()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
